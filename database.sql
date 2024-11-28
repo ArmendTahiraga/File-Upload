dc87d6aeea1f1d65222ea6dcf13b337c05296c10
Armend <armend_tahiraga@outlook.com>
2024-11-28 23:13:33 +0100

    Formated the database.sql file

--- database.sql
+++ database.sql
@@ -1,31 +1,35 @@
-create table User (
-    Id int auto_increment primary key,
+create table User
+(
+    Id       int auto_increment primary key,
     Username varchar(200) not null,
     Password varchar(200) not null,
-    Role varchar(10) not null
+    Role     varchar(10)  not null
 );
 
-create table Contract (
-    Id int auto_increment primary key,
-    UserId int not null,
+create table Contract
+(
+    Id       int auto_increment primary key,
+    UserId   int          not null,
     FileName varchar(200) not null,
-    File longblob not null,
-    ExpDate date not null,
-    foreign key (UserId) references User(Id) on delete cascade on update cascade
+    File     longblob     not null,
+    ExpDate  date         not null,
+    foreign key (UserId) references User (Id) on delete cascade on update cascade
 );
 
-create table "User" (
-    "Id" serial primary key,
+create table "User"
+(
+    "Id"       serial primary key,
     "Username" varchar(200) not null,
     "Password" varchar(200) not null,
-    "Role" varchar(10) not null
+    "Role"     varchar(10)  not null
 );
 
-create table "Contract" (
-    "Id" serial primary key,
-    "UserId" int not null,
+create table "Contract"
+(
+    "Id"       serial primary key,
+    "UserId"   int          not null,
     "FileName" varchar(200) not null,
-    "File" bytea not null,
-    "ExpDate" date not null,
-    constraint fk_user foreign key ("UserId") references "User"("Id") on delete cascade on update cascade
+    "File"     bytea        not null,
+    "ExpDate"  date         not null,
+    constraint fk_user foreign key ("UserId") references "User" ("Id") on delete cascade on update cascade
 );
